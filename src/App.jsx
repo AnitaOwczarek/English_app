a0a53ac8d7499a22a974b1aeb3866bda45b7954c
Anita Owczarek <anitaowczarek99@wp.pl>
2023-12-29 10:07:50 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './assets/Pages/Home/Home';
+import Level from './assets/Pages/Level/Level';
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/beginner' element={<Level category='beginner'/>}/>
+        <Route path='/intermediate' element={<Level category='intermediate'/>}/>
+        <Route path='/advanced' element={<Level category='advanced'/>}/>
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/Pages/Home/Home.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container'
+import './Home.css'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <h1>Choose you level!</h1>
+        <div className="home-levels-container">
+          <Container fluid="md">
+          <Row className='levels-row'>
+            <Col xs={12} sm={4}>
+              <Link to="/beginner">
+              <Button className="home-level" variant="outline-secondary" >Beginner</Button>
+              </Link>
+            </Col>
+            <Col xs={12} sm={4}>
+              <Link to="/intermediate">
+                <Button className="home-level" variant="outline-secondary">Intermediate</Button>
+              </Link>
+            </Col>
+            <Col xs={12} sm={4}>
+              <Link to="/advanced">
+                <Button className="home-level" variant="outline-secondary">Advanced</Button>
+              </Link>
+            </Col>
+          </Row>
+          </Container>
+        </div>
+        {/* <Row>
+          <Col xs={12}>
+            <div className="level" >Beginner</div>
+          </Col>
+          <Col xs={8}>
+            <div className="level">Intermediate</div>
+          </Col>
+          <Col>
+            <div className="level">Advanced</div>
+          </Col>
+        </Row> */}
+        
+        
+        
+      
+      {/* <Button variant="primary">Przycisk2</Button> */}
+    </div>
+  )
+}
+
+export default Home
